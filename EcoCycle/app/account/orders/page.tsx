5a1a1b8b2698c4b0591a13d0a5c6fabd09bed9aa
Jamesselmon <jamesselmon78169@gmail.com>
2025-05-13 13:30:42 +0700

    fix path in to order detail in orders page

--- EcoCycle/app/account/orders/page.tsx
+++ EcoCycle/app/account/orders/page.tsx
@@ -148,7 +148,7 @@ const OrdersPage = () => {
   
   // View order details
   const viewOrderDetails = (orderId: string) => {
-    router.push(`account/orders/${orderId}`);
+    router.push(`/orders/${orderId}`);
   };
   
   // Handle logout
